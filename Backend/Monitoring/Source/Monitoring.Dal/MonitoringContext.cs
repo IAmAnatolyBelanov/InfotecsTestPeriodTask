4ecec36f8d8535a956a875413328f027c696a207
INFOTECS-NT\Belanov.Anatoly <Anatoly.Belanov@infotecs.ru>
2023-06-28 11:12:38 +0300

    #1 Data access layer

---
+++ Backend/Monitoring/Source/Monitoring.Dal/MonitoringContext.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Monitoring.Dal;
+public class MonitoringContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("AppDb");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<DeviceInfo> Devices { get; set; }
+    public DbSet<LoginInfo> Logins { get; set; }
+}
+
+public class LoginInfo
+{
+    public Guid Id { get; set; }
+    public Guid DeviceId { get; set; }
+    public DeviceInfo? Device { get; set; }
+    public string UserName { get; set; } = default!;
+    public string ApplicationVersion { get; set; } = default!;
+    public DateTimeOffset DateTime { get; set; }
+}
+
+public class DeviceInfo
+{
+    public Guid Id { get; set; }
+    public OperationSystemType OperationSystemType { get; set; }
+    public string OperationSystemInfo { get; set; } = default!;
+}
+
+public enum OperationSystemType
+{
+    None,
+    Windows,
+    Linux,
+    MacOs,
+    Android,
+    Ios,
+}
