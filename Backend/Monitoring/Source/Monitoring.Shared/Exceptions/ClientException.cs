ab6c8f3dc8cfcc301e679fb53a9a47b55c8b6aee
INFOTECS-NT\Belanov.Anatoly <Anatoly.Belanov@infotecs.ru>
2023-06-30 09:07:17 +0300

    #1 Format fix

--- Backend/Monitoring/Source/Monitoring.Shared/Exceptions/ClientException.cs
+++ Backend/Monitoring/Source/Monitoring.Shared/Exceptions/ClientException.cs
@@ -8,18 +8,27 @@ public class ClientException : Exception
     /// <summary>
     /// Конструктор класса <see cref="ClientException"/>.
     /// </summary>
-    public ClientException() : base() { }
+    public ClientException()
+        : base()
+    {
+    }
 
     /// <summary>
     /// Конструктор класса <see cref="ClientException"/>.
     /// </summary>
     /// <param name="message">Сообщение об ошибке.</param>
-    public ClientException(string message) : base(message) { }
+    public ClientException(string message)
+        : base(message)
+    {
+    }
 
     /// <summary>
     /// Конструктор класса <see cref="ClientException"/>.
     /// </summary>
     /// <param name="message">Сообщение об ошибке.</param>
     /// <param name="innerException">Внутреннее исключение.</param>
-    public ClientException(string message, Exception innerException) : base(message, innerException) { }
+    public ClientException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
 }
