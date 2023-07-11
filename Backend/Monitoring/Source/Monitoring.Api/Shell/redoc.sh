887c2e2a29e1ab7fd4d80d372d67e258f39341e5
INFOTECS-NT\Belanov.Anatoly <Anatoly.Belanov@infotecs.ru>
2023-07-11 17:21:37 +0300

    #2 Format tag in docker image version

--- Backend/Monitoring/Source/Monitoring.Api/Shell/redoc.sh
+++ Backend/Monitoring/Source/Monitoring.Api/Shell/redoc.sh
@@ -3,8 +3,8 @@
 # move to directory with .sln file
 cd ./../../../
 
-docker build --pull --rm -f "./Source/Monitoring.Api/Shell/Dockerfile" -t monitoring.api:202307111712 "./"
+docker build --pull --rm -f "./Source/Monitoring.Api/Shell/Dockerfile" -t monitoring.api:2023.07.11.1712 "./"
 docker pull redocly/cli
 
-docker run --name monitoring.api -p 8081:80 -d monitoring.api:202307111712
+docker run --name monitoring.api -p 8081:80 -d monitoring.api:2023.07.11.1712
 docker run -p 8080:80 -e SPEC_URL=http://localhost:8081/swagger/v1/swagger.json redocly/redoc
