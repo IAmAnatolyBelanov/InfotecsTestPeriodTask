e50bc1f67d1d007a2caec5dabc03af3314f1718c
INFOTECS-NT\Belanov.Anatoly <Anatoly.Belanov@infotecs.ru>
2023-07-19 11:19:52 +0300

    #5 Remove unused methods

--- Backend/Monitoring/Source/Monitoring.Shared/Extensions/IEnumerableExtensions.cs
+++ Backend/Monitoring/Source/Monitoring.Shared/Extensions/IEnumerableExtensions.cs
@@ -22,32 +22,4 @@ public static class IEnumerableExtensions
     /// <returns><see langword="false"/>, если список равен <see langword="null"/> или он пуст. В противном случае <see langword="true"/>.</returns>
     public static bool HasSome<T>(this IEnumerable<T>? collection)
         => collection != null && collection.Any();
-
-    /// <summary>
-    /// Выполняет действие на каждый элемент коллекции.
-    /// </summary>
-    /// <typeparam name="T">Тип коллекции.</typeparam>
-    /// <param name="collection">Коллекция.</param>
-    /// <param name="action">Действие.</param>
-    public static void ForEach<T>(this IReadOnlyList<T> collection, Action<T> action)
-    {
-        for (var i = 0; i < collection.Count; i++)
-        {
-            action(collection[i]);
-        }
-    }
-
-    /// <summary>
-    /// Выполняет действие на каждый элемент коллекции.
-    /// </summary>
-    /// <typeparam name="T">Тип коллекции.</typeparam>
-    /// <param name="collection">Коллекция.</param>
-    /// <param name="action">Действие.</param>
-    public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
-    {
-        foreach (var item in collection)
-        {
-            action(item);
-        }
-    }
 }
